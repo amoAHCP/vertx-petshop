7b4ff09f6b283185928a69eaf0f5d6597271b639
PETE-CH <ann1na2oo5bb>
2014-03-14 23:28:54 +0100

    html

--- petstore-server/web/js/application.js
+++ petstore-server/web/js/application.js
@@ -1,3 +1,46 @@
+angular.module('Petstore', ['doowb.angular-pusher']).
+
+    config(['PusherServiceProvider',
+        function (PusherServiceProvider) {
+            PusherServiceProvider
+                .setToken('60b1ae822a161deb3f0c')
+                .setOptions({});
+        }
+    ]);
+
 function MainController($scope) {
     $scope.name = 'AngularJS';
+}
+
+var ItemListController = function($scope, $http, Pusher) {
+    $scope.items = [];
+
+    Pusher.subscribe('items', 'updated', function (item) {
+        // an item was updated. find it in our list and update it.
+        for (var i = 0; i < $scope.items.length; i++) {
+            if ($scope.items[i].id === item.id) {
+                $scope.items[i] = item;
+                break;
+            }
+        }
+    });
+
+    var retrieveItems = function () {
+        // get a list of items from the api located at '/api/items'
+        console.log('getting items');
+        $http.get('/all')
+            .success(function (items) {
+                $scope.items = items;
+            }).error(function (error){
+                console.log('error');
+            });
+    };
+
+    $scope.updateItem = function (item) {
+        console.log('updating item');
+        $http.post('/update', item);
+    };
+
+    // load the items
+    retrieveItems();
 }
