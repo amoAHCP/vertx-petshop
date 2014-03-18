fefd194dcfefeed82bdf0ac7d870a0b7e81aaa6b
PETE-CH <ann1na2oo5bb>
2014-03-18 08:12:57 +0100

    Constants

---
+++ petstore-server/src/main/java/org/jacpfx/petstore/util/GlobalConstants.java
@@ -0,0 +1,10 @@
+package org.jacpfx.petstore.util;
+
+/**
+ * @author Patrick Symmangk (pete.jacp@gmail.com)
+ *
+ */
+public class GlobalConstants {
+
+    public static final String DUMMY_PICTURE = "petstore-dummy.png";
+}
